410bf2ef95effa0f63ed44c6a640a8a674ccf45d
berishaesmaa <esmaberisha460@gmail.com>
2025-01-27 03:18:45 -0800

    .

---
+++ lesson26/main.js
@@ -0,0 +1,41 @@
+console.log("goodbye")
+
+var x=5;
+
+var a = 50 + 50;
+console.log(a)
+
+var b=20;
+var c=30;
+var d= b+ c;
+console.log(d);
+
+var e=40;
+var f=10;
+var g= e - f;
+console.log(g)
+
+var h=100;
+var i=5;
+var j=h/i
+console.log(j)
+
+var k=10;
+var l=4;
+var m= k % l;
+console.log(m);
+
+var n=99;
+n++;
+console.log(n)
+
+var o=101;
+o--;
+console.log(o)
+
+var p=20;
+var q=p**2;
+console.log(q)
+
+var x =15;
+var x+=5
